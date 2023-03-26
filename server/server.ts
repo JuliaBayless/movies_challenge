01c0d978e37933e2b4e974d398b4053170fd2de1
Julia Bayless <julia.allistair@gmail.com>
2023-03-26 17:24:55 -0500

    moved code over from branch with different origin

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
+import axios from 'axios';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('test!');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.get('/movies/:title', (req, res) => {
+    const apiKey = process.env.API_KEY;
+    const title = req.params.title;
+
+    axios.get(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(error => {
+        res.status(500).send(error);
+      });
+  });
+
+  const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
 });
