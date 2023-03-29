b5cafa936b3cd37135e7d0a289d947d3c54944f3
Julia Bayless <julia.allistair@gmail.com>
2023-03-28 21:59:03 -0500

    set up

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '@fontsource/poppins';
+import './App.css'
+
+export default function App() {
+    return (
+        <div>hello world</div>
+    );
+  }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+const queryClient = new QueryClient();
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <App />
         <ToastContainer
             position="bottom-center"
             autoClose={3000}
@@ -21,6 +26,7 @@ root.render(
             pauseOnHover
             theme="light"
           />
+          </QueryClientProvider>
         </BrowserRouter>
     </React.StrictMode>
 )
