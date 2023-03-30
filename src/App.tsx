c34f2de7f4f1c2742b3d64f3ffe70d48ab5d2da6
Julia Bayless <julia.allistair@gmail.com>
2023-03-29 21:51:14 -0500

    reusable base card and css started

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@fontsource/poppins';
 import './App.css';
+import BaseCard from './reusable-components/BaseCard';
 
 export default function App() {
   return (
-    <div>hello world</div>
+    <BaseCard />
   );
 }
