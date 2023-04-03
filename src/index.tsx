7e279748b91305f77201e8b24d8ae3f8321a1bcb
Julia Bayless <julia.allistair@gmail.com>
2023-04-03 10:46:58 -0500

    react query tool

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App';
 
 const container = document.getElementById('root')!;
@@ -13,6 +14,8 @@ root.render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        {/* turn this import on to play with react query */}
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
