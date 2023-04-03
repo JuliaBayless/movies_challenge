fecf76a2adf0cabd8d71015f9b72927bc684a5c4
Julia Bayless <julia.allistair@gmail.com>
2023-04-03 10:12:20 -0500

    added a try catch

--- server/router.js
+++ server/router.js
@@ -7,7 +7,6 @@ const router = Router();
 // search movies in OMDb
 router.get('/', async (req, res) => {
   try {
-    console.log('IN', req.query)
     const data = await fetchMovies(req.query);
     res.json(data);
   } catch (error) {
@@ -19,10 +18,17 @@ router.get('/', async (req, res) => {
 
 // grabs single movie
 router.get('/:id', async (req, res) => {
-  const apiKey = process.env.API_KEY;
-  const { id } = req.params;
-  const response = await axios.get(`http://www.omdbapi.com/?i=${id}&apikey=${apiKey}`);
-  res.json(response.data);
+  try {
+    const apiKey = process.env.API_KEY;
+    const { id } = req.params;
+    const response = await axios.get(`http://www.omdbapi.com/?i=${id}&apikey=${apiKey}`);
+    res.json(response.data);
+  } catch (error) {
+     // eslint-disable-next-line no-console
+     console.error(`Error fetching data from OMDB API: ${error}`);
+     res.status(500).json({ error: 'Error fetching data from OMDB API' });
+  }
+
 });
 
 export default router;
